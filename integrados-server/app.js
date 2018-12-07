7e133b0af0101fe6c6345c1eba7f7af9426d4dee
Luca Magalhaes <lucamagalhaes@hero99.com.br>
2018-12-07 01:57:42 -0200

    Terminando projeto

--- integrados-server/app.js
+++ integrados-server/app.js
@@ -4,16 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +26,6 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- integrados-server/routes/index.js
+++ integrados-server/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const fs = require('fs');
 
+const storage = multer.diskStorage({
+  destination: "public/images/",
+  filename: function (req, file, cb) {
+    let name = file.originalname.split(".");
+    let extension = name[name.length-1];
+    cb(null, file.fieldname + '-' + Date.now() + "." + extension);
+  }
+})
+
+const upload = multer({ storage: storage })
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readdir('public/images/', (err, items) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send({mensagem: "Serviço indisponivel no momento"});
+    }
+    const files = items.filter((item) => item !== "." && item !== "..");
+    return res.send({files, path: "public/images/"});
+  })
+});
+
+router.post('/upload', upload.single("image"), function(req, res, next) {
+  if (req.file) {
+    return res.send({ mensagem: "Arquivo salvo com sucesso!"});
+  } else {
+    return res.status(500).send({ mensagem: "Não foi possivel salvar o arquivo!" });
+  }
 });
 
 module.exports = router;
